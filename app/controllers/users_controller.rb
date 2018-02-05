e3dbc14030c14f60c66d6231a7402b4ef72a994a
Thomas 2 <thomas2@mbp-de-thomas.home>
2018-02-05 16:08:26 +0100

    creation controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'static_pages/home'
+
+  get 'static_pages/secret'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/destroy'
+
+root'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
